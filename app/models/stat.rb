72cedb9f9e7f4b4718f8919d52f691b00aaf67a6
01ghost13 <orkich@gmail.com>
2016-02-28 23:08:03 +0100

    Linked Point to Stat

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,3 +1,4 @@
 class Stat < ActiveRecord::Base
   belongs_to :student
+  has_and_belongs_to_many :points
 end
--- db/migrate/20160228220021_create_points.rb
+++ db/migrate/20160228220021_create_points.rb
@@ -1,7 +1,8 @@
 class CreatePoints < ActiveRecord::Migration
   def change
     create_table :points do |t|
-
+      t.integer :earned_points
+      
       t.timestamps null: false
     end
   end
