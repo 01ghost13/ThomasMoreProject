4f62b3d91a0b71410005f891b09f512764f99bfe
01ghost13 <orkich@gmail.com>
2016-02-29 20:49:55 +0100

    Linked Point to Interest

--- db/migrate/20160228220021_create_points.rb
+++ db/migrate/20160228220021_create_points.rb
@@ -2,7 +2,7 @@ class CreatePoints < ActiveRecord::Migration
   def change
     create_table :points do |t|
       t.integer :earned_points
-      
+      t.belongs_to :interest
       t.timestamps null: false
     end
   end
