022e7d3c05d366de0440bcd3144f22113bc9098f
01ghost13 <orkich@gmail.com>
2016-03-08 13:04:26 +0100

    Added seed

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,11 @@
 class Student < ActiveRecord::Base
   has_secure_password
   has_many :result_of_tests
-  validates :codename, presence: true, uniqueness: true, length: { in: 6..20}
+  validates :code_name, presence: true, uniqueness: true, length: { in: 6..20}
   validates :gender, presence: true, inclusion: { in: %w(1 2 3) }
-  #0 – dunno
-  #1 – Men
-  #2 – Women
+  #1 – dunno
+  #2 – Men
+  #3 – Women
 
   validates :is_active,:is_current_in_school, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,21 @@
 #Creating of administrator
 admInfo = Info.create(name: 'Jan', last_name: 'Dekelver', password_digest: '12345', phone: '89012345678', mail: 'test@mail.com', is_mail_confirmed: true)
 superAdm = Administrator.create(info_id: admInfo.id, organisation: 'Administrator', is_super: true)
+#Creating of modes
+blind_mode = Mode.create(name: 'Blind')
+one_click_mode = Mode.create(name: 'One click mode')
+#Schooling
+example_of_schooling = Schooling.create(name: 'School')
+another_example_of_schooling = Schooling.create(name: 'University')
+#Creating of seed models...
+#LAdm
+LadmInfo = Info.create(name: 'LocalAdm', last_name: 'TestLastNamr', password_digest: '020695', phone: '89023853401', mail: 'another@mail.com', is_mail_confirmed: true)
+lAdm = Administrator.create(info_id: LadmInfo.id, organisation: 'Some organisation', is_super: false)
+#Tutors
+tutorInfo = Info.create(name: 'Dmitry', last_name: 'Skvaznikov', password_digest: '020695', phone: '89023853401', mail: 'some@mail.com', is_mail_confirmed: true)
+first_tutor = Tutor.create(administrator_id: lAdm.id, info_id: tutorInfo.id)
+
+#Students
+first_student = Student.create(tutor_id: first_tutor.id, code_name: '01ghost13', gender: 2, is_active: true, password_digest: '020695', schooling_id: example_of_schooling.id, is_current_in_school: true)
+
+#Tests
