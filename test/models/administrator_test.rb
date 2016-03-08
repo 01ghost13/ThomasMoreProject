134c183d9349fdb609d29e8cae74c2d93bb48b58
01ghost13 <orkich@gmail.com>
2016-03-08 14:59:23 +0100

    Added tests to adm

--- test/models/administrator_test.rb
+++ test/models/administrator_test.rb
@@ -4,4 +4,22 @@ class AdministratorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :administrators
+  fixtures :infos
+  test "administrators fields must be non empty" do
+    adm = Administrator.new
+    assert adm.invalid?
+    assert adm.errors[:is_super].any?
+    assert adm.errors[:info_id].any?, "Shouldnt be empty"
+  end
+  test "Uniquenes of adm column" do
+    adm = Administrator.new(info_id: infos(:infoAdm).id, organisation: 'TestOrg', is_super: administrators(:admin).is_super)
+    assert adm.invalid?
+    assert adm.errors[:is_super].any?, "should be uniq"
+  end
+  test "Uniquenes of info" do
+    adm = Administrator.new(info_id: administrators(:admin).info_id, organisation: 'TestOrg', is_super: false)
+    assert adm.invalid?
+    assert adm.errors[:info_id].any?, "should be uniq"
+  end
 end
