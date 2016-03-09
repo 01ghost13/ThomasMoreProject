765ec21e8b55aa74f3f31f7051fa0cc46ac8b76b
01ghost13 <orkich@gmail.com>
2016-03-09 13:05:34 +0100

    added test to Interests and fixed the problem with this model

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,5 +1,5 @@
 class Interest < ActiveRecord::Base
   has_many :points
   has_many :pictures, :through => :picture_interests
-  validates :interest, presence: true, length: {in: 2..20}
+  validates :name, uniqueness: true, presence: true, length: {in: 2..20}
 end
--- test/models/interest_test.rb
+++ test/models/interest_test.rb
@@ -4,4 +4,15 @@ class InterestTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :interests
+  test "Empty fields" do
+    i = Interest.new
+    assert i.invalid?
+    assert i.errors[:name].any?
+  end
+  test "Uniqueness" do
+    i = Interest.new(name: interests(:test_inter).name)
+    assert i.invalid?
+    assert i.errors[:name].any?
+  end
 end
