14fecb6ea0b563043a12281e87d64786ea501937
01ghost13 <orkich@gmail.com>
2016-03-09 12:24:21 +0100

    added tests to ResultOfTest Mode

--- test/models/result_of_test_test.rb
+++ test/models/result_of_test_test.rb
@@ -4,4 +4,13 @@ class ResultOfTestTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Empty fields" do
+    res = ResultOfTest.new
+    assert res.invalid?
+    assert res.errors[:test_id].any?
+    assert res.errors[:was_in_school].any?
+    assert res.errors[:student_id].any?
+    assert res.errors[:schooling_id].any?
+    assert res.errors[:is_ended].any?
+  end
 end
