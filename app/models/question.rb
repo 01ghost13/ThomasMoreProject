00fe5d6c62043f719d927044d00404382374bd11
01ghost13 <orkich@gmail.com>
2016-03-09 12:09:04 +0100

    Added test to Questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ActiveRecord::Base
-  validates :is_tutorial, presence: true
+  validates :is_tutorial, :test_id, :picture_id, presence: true
   validates :number, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
