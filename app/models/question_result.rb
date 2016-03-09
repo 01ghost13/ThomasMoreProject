88f5eefc84f47854dbcc7477b1796840e3695bdc
01ghost13 <orkich@gmail.com>
2016-03-09 10:41:02 +0100

    fixed problem with code

--- app/models/question_result.rb
+++ app/models/question_result.rb
@@ -1,4 +1,4 @@
 class QuestionResult < ActiveRecord::Base
   validates :number, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :start, :end, :was_rewrited, :was_checked,:result_of_test_id presence: true
+  validates :start, :end, :was_rewrited, :was_checked,:result_of_test_id, presence: true
 end
