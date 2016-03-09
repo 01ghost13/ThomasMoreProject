72f6559bd19bd75343e01617ffe7cf9166198f54
01ghost13 <orkich@gmail.com>
2016-03-09 11:08:38 +0100

    Added tests to Test Model

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ActiveRecord::Base
   has_many :questions
   has_many :result_of_tests
   validates :name,:description,:version, presence: true
-  validates :name, length: {in: 5..20}
+  validates :name, uniqueness: true, length: {in: 5..20}
   validates :description, length: {in: 5..50}
   validates :version, numericality: true, length: {in: 1..10}
 end
--- test/models/test_test.rb
+++ test/models/test_test.rb
@@ -4,4 +4,17 @@ class TestTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :tests
+  test 'Empty fields' do
+    some_test = Test.new
+    assert some_test.invalid?
+    assert some_test.errors[:name].any?
+    assert some_test.errors[:description].any?
+    assert some_test.errors[:version].any?
+  end
+  test 'Uniq name' do
+    some_test = Test.new(name: tests(:first_test).name, description: 'blablalab', version: '1.0')
+    assert some_test.invalid?
+    assert some_test.errors[:name].any?
+  end
 end
