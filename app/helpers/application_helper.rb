84a1e0a2c9c08de1bab649a8ac4fef388fa873f4
01ghost13 <orkich@gmail.com>
2017-04-06 23:13:56 +0300

    Some fixes
    Removed js include in pictures/new
    Replaced helper metod full_title from admins to app helps
    Fixed bug with self deletion of SA

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
   def link_to_image(image_path, target_link,options={})
     link_to(image_tag(image_path,class: "img-responsive"), target_link, options)
   end
+
+  def full_title(page_title = '')
+    base_title = "AitScore"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
