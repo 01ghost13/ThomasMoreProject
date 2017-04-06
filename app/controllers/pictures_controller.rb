5b809dbd9434ad9e3cc9ee4946a4f96c37c4c366
01ghost13 <orkich@gmail.com>
2017-04-06 18:53:11 +0300

    Added rights to Picture actions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,7 @@
 class PicturesController < ApplicationController
+  before_action :check_log_in
+  before_action :check_rights
+  
   def index
     @pictures = Picture.order(:created_at).reverse_order.page(params[:page]).per(5)
   end
@@ -53,4 +56,21 @@ class PicturesController < ApplicationController
           :description, :image, picture_interests_attributes: [
           :interest_id, :earned_points, :_destroy, :id])
     end
+  def check_log_in
+    unless logged_in?
+      flash[:warning] = 'Only registrated people can see this page.'
+      #Redirecting to home page
+      redirect_to :root
+    end
+  end
+
+  #Callback for checking rights
+  def check_rights
+    #Only SA
+    unless is_super?
+      flash[:warning] = 'You have no access to this page.'
+      #Redirect
+      redirect_to current_user
+    end
+  end
 end
