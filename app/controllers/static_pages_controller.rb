fe7dd77e121691b1e97e3b901a7de6f4a208be21
01ghost13 <orkich@gmail.com>
2017-04-16 19:15:30 +0300

    Added about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,43 @@
 class StaticPagesController < ApplicationController
   def log_in
-    
+
+  end
+
+  def about
+    added = %w(
+    All\ pictures\ should\ have\ text\ description
+    Test\ should\ have\ progress\ bar
+    Every\ step\ in\ the\ test\ should\ have\ 1\ picture\ instead\ of\ 3
+    Test\ should\ have\ several\ navigation\ buttons:\ Exit,\ Step\ back,\ Like\ ,\ Don’t\ like
+    Every\ picture\ must\ represent\ several\ fields\ of\ interests
+    Administrator\ for\ managing\ users
+    Profiles\ for\ students,\ teachers
+    Saving\ and\ managing\ all\ results\ of\ tests
+    Information\ which\ must\ be\ saved:\ date,\ time\ of\ test,\ time\ between\ answers,\ the\ fact\ of\ reanswering
+    Table\ with\ results
+    Switching\ off\ student\ accounts\ instead\ of\ deleting\ from\ DB
+    Creation\ of\ tests,\ interests
+    )
+    will_be_added = %w(
+    Graph\ with\ results
+    Protection\ from\ spam-bots
+    Highlighting\ of\ buttons
+    Confirmation\ of\ registration\ via\ e-mail
+    Recovering\ of\ account\ via\ e-mail
+    Student\ and\ teacher\ search\ page
+    Export\ to\ excel
+    Sound\ description\ of\ images,\ for\ people\ with\ poor\ eyesight
+    )
+    @arr = {}
+    added.each do |el|
+      @arr[el] = true
+    end
+    will_be_added.each do |el|
+      @arr[el] = false
+    end
+  end
+
+  def contacts
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  get 'about' => 'static_pages#about'
+  get 'contacts' => 'static_pages#contacts'
 
   #Interests pages
   resources :interests, only: [:index, :destroy]
