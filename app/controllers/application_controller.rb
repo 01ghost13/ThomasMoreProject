9beda94b937141e452d370d5e46c64a81f653a92
01ghost13 <orkich@gmail.com>
2017-06-06 17:10:59 +0300

    Refactored Mail Controller and app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  #Callback for checking session
+  def check_log_in
+    unless logged_in?
+      flash[:warning] = 'Only registered people can see this page.'
+      #Redirecting to home page
+      redirect_to :root
+    end
+  end
+
+  #Callback for checking log out session
+  def check_log_out
+    if logged_in?
+      flash[:warning] = 'This pages only for logged out users'
+      #Redirecting to home page
+      redirect_to :root
+    end
+  end
 end
--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -2,7 +2,7 @@ class MailController < ApplicationController
   before_action :check_log_in, only: [:send_confirmation_again]
   before_action :check_log_out, except: [:send_confirmation_again, :confirmation_email]
 
-  #Page for confirmation email
+  #Action for confirmation email
   def confirmation_email
     info = Info.find_by_confirm_token(params[:id])
     if info && !params[:id].nil?
@@ -14,7 +14,9 @@ class MailController < ApplicationController
     redirect_to :root
   end
 
+  #Action for resending confirmation email
   def send_confirmation_again
+    #If not student and sending for himself
     if session[:user_type] != 'student' && params[:id].to_i == session[:user_id]
       user_info = Info.find(params[:id])
       if user_info.is_mail_confirmed?
@@ -31,9 +33,9 @@ class MailController < ApplicationController
 
   #Page for sending reset link
   def forgot_password
-
   end
 
+  #Action for sending email with reset link
   def submit_forgot_password
     info = Info.find_by_mail(params[:mail])
     if info
@@ -49,10 +51,11 @@ class MailController < ApplicationController
 
   #Page for reset password
   def reset_password
-
+    #Saving reset-token for hidden html field
     @token = params[:id]
   end
 
+  #Action for reset password
   def submit_reset_password
     info = Info.find_by_reset_token(params[:reset_token])
     if info && !params[:reset_token].nil?
@@ -67,24 +70,6 @@ class MailController < ApplicationController
     else
       flash[:danger] = "Sorry. User doesn't exist"
       redirect_to reset_password_url(id: params[:reset_token])
-    end
-  end
-
-  #Callback for checking session
-  def check_log_in
-    unless logged_in?
-      flash[:warning] = 'Only registered people can see this page.'
-      #Redirecting to home page
-      redirect_to :root
-    end
-  end
-
-  #Callback for checking log out session
-  def check_log_out
-    if logged_in?
-      flash[:warning] = 'This pages only for logged out users'
-      #Redirecting to home page
-      redirect_to :root
     end
   end
 end
