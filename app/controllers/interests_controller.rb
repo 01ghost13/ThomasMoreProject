39406c89660339fd751e96c4350b746a3fe6b8ab
01ghost13 <orkich@gmail.com>
2017-06-06 19:18:08 +0300

    Refactored interests

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,29 +1,26 @@
 class InterestsController < ApplicationController
   before_action :check_log_in
-  before_action :check_rights
+  before_action :check_super_admin
 
+  #Action for creation Interest
   def create
     @interest = Interest.new(interest_params)
     if @interest.save
       flash[:success] = 'Interest added!'
       redirect_to interests_path
     else
-      @interests_list = Interest.order(:created_at).reverse_order.page(params[:page]).per(5)
-      @user = @interest
-      render :index
+      load_info_for_page(@interest, false)
     end
   end
 
+  #Action for updating interest
   def update
     interest = Interest.find(params[:interest][:id])
     if interest.update(interest_params)
       flash[:success] = 'Interest updated!'
       redirect_to interests_path
     else
-      @interests_list = Interest.order(:created_at).reverse_order.page(params[:page]).per(5)
-      @interest = Interest.new
-      @user = interest
-      render :index
+      load_info_for_page(interest, true)
     end
   end
 
@@ -33,40 +30,27 @@ class InterestsController < ApplicationController
       flash[:success] = 'Interest deleted!'
       redirect_to interests_path
     else
-      @interests_list = Interest.order(:created_at).reverse_order.page(params[:page]).per(5)
-      @interest = Interest.new
-      @user = interest
-      render :index
+      load_info_for_page(interest, true)
     end
   end
 
   def index
     @interests_list = Interest.order(:created_at).reverse_order.page(params[:page]).per(5)
     @interest = Interest.new
-    @user = @interest
   end
-
+  ##########################################################
+  #Private methods
   private
-    def interest_params
-      params.require(:interest).permit(:name, :id)
-    end
-
-  #Callback for checking session
-  def check_log_in
-    unless logged_in?
-      flash[:warning] = 'Only registrated people can see this page.'
-      #Redirecting to home page
-      redirect_to :root
-    end
+  #Attributes for forms
+  def interest_params
+    params.require(:interest).permit(:name, :id)
   end
 
-  #Callback for checking rights
-  def check_rights
-    #Only SA
-    unless is_super?
-      flash[:warning] = 'You have no access to this page.'
-      #Redirect
-      redirect_to current_user
-    end
+  #Loads interest to page. if create_new true - creates new interest for form
+  def load_info_for_page(interest, create_new)
+    @interests_list = Interest.order(:created_at).reverse_order.page(params[:page]).per(5)
+    @interest = Interest.new if create_new
+    @user = interest
+    render :index
   end
 end
