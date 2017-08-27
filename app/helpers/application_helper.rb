4149ee7e3dfb515dffd9e6b49114c6613f06cbd1
01ghost13 <orkich@gmail.com>
2017-08-27 19:09:52 +0300

    Added using of date converter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,10 @@ module ApplicationHelper
   end
 
   #Adds date_text class to date, so it could be converted via js
-  def date_to_local(date)
+  def date_to_local(date=nil)
+    if date.nil? || date == ''
+      return
+    end
     converted_date = date.to_s(:iso8601)
     html_string = "<div class=\"date_to_local\">#{converted_date}</div>".html_safe
   end
