253e17424663dc0606fcbfb4077fa21ef7e8be1f
01ghost13 <orkich@gmail.com>
2018-10-21 19:39:33 +0300

    Added new relations; Removed old

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,27 +1,12 @@
 class Picture < ActiveRecord::Base
   has_many :picture_interests, inverse_of: :picture, dependent: :destroy
   has_many :interests, :through => :picture_interests
-  accepts_nested_attributes_for :picture_interests, reject_if: :all_blank, allow_destroy: true
   has_many :questions, dependent: :destroy
-  #Settigns for production
-  has_attached_file :image,
-                    path: ':class/:style/:id_:hash.:extension',
-                    url: '/:class/:style/:id_:hash.:extension',
-                    hash_secret: 'TWILIGHT_IS_BEST_PONY',
-                    styles: {thumb: ['300x300>']},
-                    storage: :cloudinary if Rails.env.production?
-  #Settings for dev
-  has_attached_file :image,
-                    path: ':rails_root/public/system/:class/:style/:id_:hash.:extension',
-                    url: '/system/:class/:style/:id_:hash.:extension',
-                    hash_secret: 'TWILIGHT_IS_BEST_PONY',
-                    styles: {thumb: ['300x300>']} if Rails.env.development?
 
+  # has_one_attached :image
+  accepts_nested_attributes_for :picture_interests, reject_if: :all_blank, allow_destroy: true
 
   validates :description, presence: true, length: {in: 5..50}
-  validates_attachment :image, presence: true, size: {less_than: 2.megabytes}
-  validates_attachment_content_type :image, content_type: /\Aimage/
-  validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
   validates :picture_interests, nested_attributes_uniqueness: {field: :interest_id}
 
   #returns related interests to a picture
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_145118) do
+ActiveRecord::Schema.define(version: 2018_10_21_153728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,10 +87,6 @@ ActiveRecord::Schema.define(version: 2018_10_21_145118) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "question_results", id: :serial, force: :cascade do |t|
