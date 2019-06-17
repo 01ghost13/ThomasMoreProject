fe4e2a7621e5aab4cf2b80e99089bf061499c29c
Unknown <orkich@gmail.com>
2019-06-17 21:02:52 +0400

    fix deleting tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ActiveRecord::Base
   after_save :set_outdated
 
-  has_many :questions, inverse_of: :test, dependent: :delete_all
-  has_many :result_of_tests, dependent: :restrict_with_error
+  has_many :questions, inverse_of: :test, dependent: :destroy
+  has_many :result_of_tests, dependent: :destroy
   accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 
   validates :name,:description,:version, presence: true
