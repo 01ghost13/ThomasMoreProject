d4c927ac9e70c36493164ba64f139dcbddec89ef
Skvaznikov <orkich@gmail.com>
2020-01-13 22:21:19 +0400

    Fix picture-interest validation

--- app/models/picture_interest.rb
+++ app/models/picture_interest.rb
@@ -14,12 +14,20 @@
 class PictureInterest < ActiveRecord::Base
   #Joins table Picture and Interest for many-to-many
   belongs_to :interest
-  belongs_to :picture
-  belongs_to :youtube_link
+  belongs_to :picture, optional: true
+  belongs_to :youtube_link, optional: true
 
   validates :earned_points, numericality: { only_integer: true, greater_than: -1, less_than: 6 }
   validates :interest,:earned_points, presence: true
   validates_associated :picture, allow_blank: true
+  validates_associated :youtube_link, allow_blank: true
   validates_uniqueness_of :interest_id, scope: :picture_id
+  validate :belongings
 
+  private
+    def belongings
+      if picture.blank? && youtube_link.blank?
+        errors[:base] << 'You need specify either picture or youtube link'
+      end
+    end
 end
